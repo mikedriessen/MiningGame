b613fc2b7b7bfbd093a975370174b89364fa4d58
Luca Yavuzok <l.yavuzok@sintlucasedu.nl>
2019-05-15 13:38:36 +0200

    raycast epic

---
+++ Assets/_Scripts/Artifact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artifact : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                Debug.Log("YEET");
+            if (hit.collider.gameObject.tag == "Artifact")
+            {
+                Debug.Log("ARTIFACT");
+                //Destroy(gameObject. >);
+            }
+        }
+    }
+}
--- Assets/playerMove.cs
+++ Assets/playerMove.cs
@@ -29,6 +29,7 @@ public class playerMove : MonoBehaviour
 
     }
 
+
     public void swipeUp()
     {
         getSwipe = true;
